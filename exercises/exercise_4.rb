7df5a075191a3af97e268c0660d6bcbf0ef33a73
ysycecilia <ysycecilia@gmail.com>
2021-07-21 15:04:40 -0400

    query a subset by given conditions

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+
+store = Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.find_each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+@womens_stores.find_each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
