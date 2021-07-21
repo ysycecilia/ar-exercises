171040843a9f6bb42bbed0b86139dcbdd102777b
ysycecilia <ysycecilia@gmail.com>
2021-07-21 15:33:39 -0400

    add relation to class and new records to table employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Elbert", last_name: "Butler", hourly_rate: 42)
+@store1.employees.create(first_name: "Adrian", last_name: "Huff", hourly_rate: 54)
+@store2.employees.create(first_name: "Kenneth", last_name: "Gregory", hourly_rate: 28)
+@store2.employees.create(first_name: "Walter", last_name: "Pratt", hourly_rate: 33)
+@store2.employees.create(first_name: "Ada", last_name: "Palmer", hourly_rate: 62)
