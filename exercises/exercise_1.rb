8324f390dcb5e5dabc950731180ce3ec3441d6f9
ysycecilia <ysycecilia@gmail.com>
2021-07-21 14:02:05 -0400

    create 3 store records

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,10 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+store = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true)
+
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true)
+
+puts Store.all.count
